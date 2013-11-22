fd44b468169d1202a758faccea052a09483ceb77
Huexxx <huexxx@gmail.com>
2013-11-22 10:41:25 +0100

    Embed Superuser

--- products/common.mk
+++ products/common.mk
@@ -23,3 +23,7 @@ PRODUCT_COPY_FILES += \
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Embed superuser
+SUPERUSER_EMBEDDED := true
+
