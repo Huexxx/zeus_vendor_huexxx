10e4b8c4cacec329173f963593c709ae430817a0
Huexxx <huexxx@gmail.com>
2014-11-16 21:29:10 +0100

    Initial Android L vendor

--- products/huexxx_hammerhead.mk
+++ products/huexxx_hammerhead.mk
@@ -4,9 +4,6 @@ $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 # Inherit common product files.
 $(call inherit-product, vendor/huexxx/products/common.mk)
 
-# Video files
-$(call inherit-product-if-exists, frameworks/base/data/videos/VideoPackage2.mk)
-
 # Setup device specific product configuration.
 PRODUCT_NAME := huexxx_hammerhead
 PRODUCT_BRAND := google
@@ -15,12 +12,13 @@ PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:4.4/KRT16M/893803:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 4.4 KRT16M 893803 release-keys"
+    PRODUCT_NAME=hammerhead
 
 PRODUCT_PACKAGES += \
-    Launcher3
-
-PRODUCT_PACKAGES += \
+    CellBroadcastReceiver \
+    com.android.nfc_extras \
     Stk
+
+# Add Nexus5 Android L bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/huexxx/prebuilts/bootanimation.zip:system/media/bootanimation.zip
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,1 @@
-add_lunch_combo huexxx_emulator-eng
 add_lunch_combo huexxx_hammerhead-user
