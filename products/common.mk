ab31c706ec2ea4a9ca58d1396085e57a55043260
Huexxx <huexxx@gmail.com>
2014-11-21 11:09:21 +0100

    Add Google Music & MusicFX

--- products/common.mk
+++ products/common.mk
@@ -167,6 +167,11 @@ PRODUCT_COPY_FILES += \
     vendor/huexxx/prebuilts/maps_libcrashreporter.so:system/app/Maps/lib/arm/libcrashreporter.so \
     vendor/huexxx/prebuilts/libgmm-jni.so:system/app/Maps/lib/arm/libgmm-jni.so
 
+# Add Google Music2 & MusicFX
+PRODUCT_PACKAGES += \
+    Music2 \
+    MusicFX
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
